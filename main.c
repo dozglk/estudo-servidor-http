668840029ba527d3cf03a29d9d9b8c56feaaa965
Allan dozGLK <jeffersonafsouza@gmail.com>
2025-01-10 21:15:03 -0300

    ajuste branch

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void){
+    int a = 2;
+    printf("Alou Mundo!!!");
+    printf("Alou Mundo!!!");
+    printf("Alou Mundo!!!");
 
-    printf("Alou Mundo!!!");
-    printf("Alou Mundo!!!");
-    printf("Alou Mundo!!!");
-    
     return 0;
 
 }
